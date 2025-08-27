3b62d48128d85b66e7a5e258c9d00b171c15057d
Agustin Gonzalez <agustin.elias.1000@gmail.com>
2025-08-27 13:28:08 -0300

    GET /recluta cambiado a GET /reclutados

--- mini-api/routes/api.php
+++ mini-api/routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ReclutaController;
 
-Route::get('/recluta', [ReclutaController::class, 'getAll']);
+Route::get('/reclutados', [ReclutaController::class, 'getAll']);
 Route::post('/recluta', [ReclutaController::class, 'store']);
 
